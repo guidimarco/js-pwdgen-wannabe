a0aaec2922b4edc9a9bc959d02464163ad900809
Marco Guidi <mark.88g@gmail.com>
2020-10-12 19:26:50 +0200

    realizzati prompt

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+// VARIABILI
+
+var UserFirstName
+var UserLastName
+var UserFavouriteColor
+
+// PROMPT nome - first name
+
+var UserFirstName = prompt("Come ti chiami?");
+console.log("Nome: " + UserFirstName);
+
+// PROMPT cognome - last name
+
+var UserLastName = prompt("Qual'è il tuo cognome?");
+console.log("Cognome: " + UserLastName);
+
+// PROMPT colore preferito - favourite color
+
+var UserFavouriteColor = prompt("Qual'è il tuo colore preferito?");
+console.log("Colore preferito: " + UserFavouriteColor);
