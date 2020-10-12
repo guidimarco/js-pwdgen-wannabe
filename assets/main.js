c7dcc0a553996795498dc869d9e2c954f659cefd
Marco Guidi <mark.88g@gmail.com>
2020-10-12 20:01:12 +0200

    realizzata password (fine js)

--- assets/main.js
+++ assets/main.js
@@ -10,32 +10,59 @@ var UserFavouriteColor
 var FavouriteColorParagraph
 
 var PasswordParagraph
+var RandomNumber
 
+// -----
 // FIRST NAME
 
+// prompt
 var UserFirstName = prompt("Come ti chiami?");
 console.log("Nome: " + UserFirstName);
 
+// getElement paragraph
 var FirstNameParagraph = document.getElementById("FirstName").innerHTML;
 console.log("Contenuto del paragrafo \"FirstName\": " + FirstNameParagraph);
 
+// riscrittura
+document.getElementById("FirstName").innerHTML = FirstNameParagraph + UserFirstName + ".";
+
+// -----
 // LAST NAME
 
+// prompt
 var UserLastName = prompt("Qual'è il tuo cognome?");
 console.log("Cognome: " + UserLastName);
 
+// getElement paragraph
 var LastNameParagraph = document.getElementById("LastName").innerHTML;
 console.log("Contenuto del paragrafo \"LastName\": " + LastNameParagraph);
 
+// riscrittura
+document.getElementById("LastName").innerHTML = LastNameParagraph + UserLastName + ".";
+
+// -----
 // FAVOURITE COLOR
 
+// prompt
 var UserFavouriteColor = prompt("Qual'è il tuo colore preferito?");
 console.log("Colore preferito: " + UserFavouriteColor);
 
+// getElement paragraph
 var FavouriteColorParagraph = document.getElementById("FavouriteColor").innerHTML;
 console.log("Contenuto del paragrafo \"FavouriteColor\": " + FavouriteColorParagraph);
 
-// PASSWORD
+// riscrittura
+document.getElementById("FavouriteColor").innerHTML = FavouriteColorParagraph + UserFavouriteColor + ".";
 
+// -----
+// PASSWORD E NUMBER
+
+// getElement paragraph
 var PasswordParagraph = document.getElementById("Password").innerHTML;
 console.log("Contenuto del paragrafo \"Password\": " + PasswordParagraph);
+
+// number
+var RandomNumber = 20;
+
+// riscrittura
+document.getElementById("Password").innerHTML = PasswordParagraph + UserFirstName + UserLastName + UserFavouriteColor + RandomNumber;
