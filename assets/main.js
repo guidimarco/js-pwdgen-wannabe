0c1f71bbe3c4e7e877a87b9f00d5f4fc91c17ccb
Marco Guidi <mark.88g@gmail.com>
2020-10-12 19:45:39 +0200

    realizzati getElement

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,41 @@
 // VARIABILI
 
 var UserFirstName
-var UserLastName
-var UserFavouriteColor
+var FirstNameParagraph
 
-// PROMPT nome - first name
+var UserLastName
+var LastNameParagraph
+
+var UserFavouriteColor
+var FavouriteColorParagraph
+
+var PasswordParagraph
+
+// FIRST NAME
 
 var UserFirstName = prompt("Come ti chiami?");
 console.log("Nome: " + UserFirstName);
 
-// PROMPT cognome - last name
+var FirstNameParagraph = document.getElementById("FirstName").innerHTML;
+console.log("Contenuto del paragrafo \"FirstName\": " + FirstNameParagraph);
+
+// LAST NAME
 
 var UserLastName = prompt("Qual'è il tuo cognome?");
 console.log("Cognome: " + UserLastName);
 
-// PROMPT colore preferito - favourite color
+var LastNameParagraph = document.getElementById("LastName").innerHTML;
+console.log("Contenuto del paragrafo \"LastName\": " + LastNameParagraph);
+
+// FAVOURITE COLOR
 
 var UserFavouriteColor = prompt("Qual'è il tuo colore preferito?");
 console.log("Colore preferito: " + UserFavouriteColor);
+
+var FavouriteColorParagraph = document.getElementById("FavouriteColor").innerHTML;
+console.log("Contenuto del paragrafo \"FavouriteColor\": " + FavouriteColorParagraph);
+
+// PASSWORD
+
+var PasswordParagraph = document.getElementById("Password").innerHTML;
+console.log("Contenuto del paragrafo \"Password\": " + PasswordParagraph);
